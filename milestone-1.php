001a82c2012f5dc617f7a35f13d76fae4322f6de
Robert Engelbert <Robe32002@yahoo.com>
2016-07-13 19:22:53 -0600

    Added a professional goal to my portfolio site

--- milestone-1.php
+++ milestone-1.php
@@ -1,4 +1,4 @@
-<!DOCTYPE htmml>
+<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="utf-8" />
@@ -11,6 +11,8 @@
 		<h2>Purpose</h2>
 		<p>This is my portfolio, where I can show my best work.</p>
 		<h2>Audience</h2>
-		<p>This site is designed to target driven I.T. professionals,start-ups and recruiters, that need a talented fullstack developer.</p>
+		<p>This site is designed to target driven I.T. professionals, start-ups and recruiters, that need a talented fullstack developer.</p>
+		<h2>Goal</h2>
+		<p>The goal of this site is to establish my professional brand, and give potential employers and collaborators an opportunity to view my work, and contact me with employment opportunities.</p>
 	</body>
 </html>
