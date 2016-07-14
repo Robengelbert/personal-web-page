46c6ed086ac5713d5163d3b5c589d155ce8b1b33
Robert Engelbert <Robe32002@yahoo.com>
2016-07-13 19:12:55 -0600

    Added a target audience to my portfolio site

--- milestone-1.php
+++ milestone-1.php
@@ -10,5 +10,7 @@
 		</header>
 		<h2>Purpose</h2>
 		<p>This is my portfolio, where I can show my best work.</p>
+		<h2>Audience</h2>
+		<p>This site is designed to target driven I.T. professionals,start-ups and recruiters, that need a talented fullstack developer.</p>
 	</body>
 </html>
