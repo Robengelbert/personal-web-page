0580eb827ab19d9014cc00c0d4431801808ed86e
Robert Engelbert <Robe32002@yahoo.com>
2016-07-13 19:42:06 -0600

    Added the beginning to the  persona portion of the site.

--- milestone-1.php
+++ milestone-1.php
@@ -8,8 +8,13 @@
 		<header>
 		<h1>Professional portfolio</h1>
 		</header>
+		<!-- List a purpose a potential audience and goals for my portfolio in <p> tags -->
 		<p><strong>Purpose:</strong>This is my portfolio, where I can show my best work.</p>
 		<p><strong>Audience:</strong>This site is designed to target driven I.T. professionals, start-ups and recruiters, that need a talented fullstack developer.</p>
 		<p><strong>Goal:</strong>The goal of this site is to establish my professional brand, and give potential employers and collaborators an opportunity to view my work, and contact me with employment opportunities.</p>
+		<!-- Create a persona  -->
+		<h2>Persona</h2>
+		<p><strong>Name:</strong>Robert Engelbert</p>
+		<p><strong>Age:</strong>32</p>
 	</body>
 </html>
