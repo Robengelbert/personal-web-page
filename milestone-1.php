b10623b74e085e091568b06cb7d15d3629395d27
Robert Engelbert <Robe32002@yahoo.com>
2016-07-13 19:02:16 -0600

    basic html structure with a header and a purpose for my page.

---
+++ milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE htmml>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Professional portfolio page</title>
+	</head>
+	<body>
+		<header>
+		<h1>Professional portfolio</h1>
+		</header>
+		<h2>Purpose</h2>
+		<p>This is my portfolio, where I can show my best work.</p>
+	</body>
+</html>
