00c55b7793c70ddbfc23cc11dda126d908684780
Robert Engelbert <Robe32002@yahoo.com>
2016-07-13 19:31:13 -0600

    Changed page layout to include purpose, audience and gaol in the p tag

--- milestone-1.php
+++ milestone-1.php
@@ -8,11 +8,8 @@
 		<header>
 		<h1>Professional portfolio</h1>
 		</header>
-		<h2>Purpose</h2>
-		<p>This is my portfolio, where I can show my best work.</p>
-		<h2>Audience</h2>
-		<p>This site is designed to target driven I.T. professionals, start-ups and recruiters, that need a talented fullstack developer.</p>
-		<h2>Goal</h2>
-		<p>The goal of this site is to establish my professional brand, and give potential employers and collaborators an opportunity to view my work, and contact me with employment opportunities.</p>
+		<p><strong>Purpose:</strong>This is my portfolio, where I can show my best work.</p>
+		<p><strong>Audience:</strong>This site is designed to target driven I.T. professionals, start-ups and recruiters, that need a talented fullstack developer.</p>
+		<p><strong>Goal:</strong>The goal of this site is to establish my professional brand, and give potential employers and collaborators an opportunity to view my work, and contact me with employment opportunities.</p>
 	</body>
 </html>
