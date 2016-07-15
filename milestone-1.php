6f626ef288d41aa292f3cc19fbf51fd9c0ed780c
Robert Engelbert <Robe32002@yahoo.com>
2016-07-14 19:06:51 -0600

    Create a persona for milestone 1 of my portfolio project.

--- milestone-1.php
+++ milestone-1.php
@@ -8,13 +8,18 @@
 		<header>
 		<h1>Professional portfolio</h1>
 		</header>
-		<!-- List a purpose a potential audience and goals for my portfolio in <p> tags -->
+		<!-- List a purpose, a potential audience, and goals for my portfolio in <p> tags -->
 		<p><strong>Purpose:</strong>This is my portfolio, where I can show my best work.</p>
 		<p><strong>Audience:</strong>This site is designed to target driven I.T. professionals, start-ups and recruiters, that need a talented fullstack developer.</p>
 		<p><strong>Goal:</strong>The goal of this site is to establish my professional brand, and give potential employers and collaborators an opportunity to view my work, and contact me with employment opportunities.</p>
 		<!-- Create a persona  -->
 		<h2>Persona</h2>
-		<p><strong>Name:</strong>Robert Engelbert</p>
-		<p><strong>Age:</strong>32</p>
+		<p><strong>Name:</strong>Carsten Hyldahl</p>
+		<p><strong>Age:</strong>38</p>
+		<p><strong>Profession:</strong>CTO at <a href="https://www.startupers.com/jobs/liveintent/7194-web-developer">LiveIntent</a> a company that is looking for a web developer, and offers a smarter way to market and advertise in email. </p>
+		<p><strong>Technology:</strong>Uses a HP Pavilion desktop and a hp notebook with windows 10 at home, and an LG Ultimate 2 data phone.</p>
+		<p><strong>Attitudes and Behaviors:</strong>Carsten doesn't have a lot of time, he's super busy because he's short on developers. He looked at my resume and portfolio, and he likes it. He put me on a short list of potential developers to hire.</p>
+		<p><strong>Frustrations and Needs:</strong> Carsten needs to hire someone with top-tier skills, who is willing to start at a mid to low level salary. He also needs to make sure they're a good fit for<a href="https://liveintent.com/"> LiveIntent.</a>Since he's in New York he has got a ton of people applying for the job. Carsten needs to find the right person for the job in 2 weeks or sooner.</p>
+		<p><strong>Goals:</strong>Carsten is short on time, and needs to find the right candidate for the job within a few minutes of looking at there portfolio, and resume.</p>
 	</body>
 </html>
