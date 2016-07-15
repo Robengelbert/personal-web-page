78e186a76d54476ab3c0d5619de9aaf9cf80718a
Robert Engelbert <Robe32002@yahoo.com>
2016-07-14 19:25:59 -0600

    Add a comment for milestone 1 of my portfolio project.

--- milestone-1.php
+++ milestone-1.php
@@ -21,6 +21,7 @@
 		<p><strong>Attitudes and Behaviors:</strong>Carsten doesn't have a lot of time, he's super busy because he's short on developers. Carsten loves to use social media to check potential candidates for positions at LiveIntent.</p>
 		<p><strong>Frustrations and Needs:</strong> Carsten needs to hire someone with top-tier skills, who is willing to start at a mid to low level salary. He also needs to make sure they're a good fit for<a href="https://liveintent.com/"> LiveIntent.</a>Since he's in New York he has got a ton of people applying for the job. Carsten needs to find the right person for the job in 2 weeks or sooner.</p>
 		<p><strong>Goals:</strong>Carsten is short on time, and needs to find the right candidate for the job within a few minutes of looking at there portfolio, and resume.</p>
+		<!-- Create a use case -->
 		<h2>Use Case</h2>
 		<p>Carsten Hyldahl received a cover letter and resume from me, in regards to the web developer job at LiveIntent. He looked at my resume and portfolio, and he likes it. He put me on a short list of potential developers to hire.Carsten needs to screen me and the other candidates for the job before the end of the day.</p>
 	</body>
