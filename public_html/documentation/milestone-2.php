cd78476e612f62a97e1b4d5c45a77a8c371471a7
Robert Engelbert <Robe32002@yahoo.com>
2016-07-21 19:23:38 -0600

    Create wireframes and site map for milestone 2 of my pwp.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset ="utf-8" />
+		<title>milestone-2</title>
+	</head>
+	<body>
+		<header>
+			<h1>Professional portfolio</h1>
+		</header>
+		<h2>Site map</h2>
+		<ul>
+			<li>Home</li>
+			<li>Portfolio</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+		<p>I want to do a single page and z-index the header so that the rest of the page will scroll behind it. The navigation will be linked to the different parts of the page so you can jump around to the parts you want or need to view. </p>
+		<h2>Mobile wireframe</h2>
+		<img src="../images/wireframe-1.png" alt="Mobile wireframe for my portfolio." />
+		<h2>Desktop wireframe</h2>
+		<img src="../images/wireframe-2.png" alt="Desktop wireframe for my portfolio." />
+	</body>
+</html>
