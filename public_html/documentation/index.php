226ee2e54ab1c46ca4542c9d669e3037fd39f0c8
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 19:25:17 -0600

    Added links to bootstrap css and javascript.

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 	<html lang="en">
 	<head>
+		<!-- Required meta tags always come first -->
 		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+
+
 		<title>robertengelbert.com</title>
 	</head>
 	<body>
-		
+		<!-- jQuery first, then Bootstrap JS. -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
