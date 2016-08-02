32287bf0af30806cb95155e05de5b1802813934b
Robert Engelbert <Robe32002@yahoo.com>
2016-08-01 19:28:50 -0600

    Added a link for custom css.

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -8,7 +8,8 @@
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-
+		<!-- custom css -->
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
 
 		<title>robertengelbert.com</title>
 	</head>
