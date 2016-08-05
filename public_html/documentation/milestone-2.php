98d22a361bbe94f0a0b7c6fb45318b022665c092
Robert Engelbert <Robe32002@yahoo.com>
2016-08-04 19:37:51 -0600

    Added a new wire frame for desktop and mobile devices to my milestone-2 page.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -17,8 +17,8 @@
 		</ul>
 		<p>I want to do a single page with a fixed header so that the rest of the page will scroll behind it. The navigation will be linked to the different parts of the page so you can jump around to the parts you want or need to view. </p>
 		<h2>Mobile wireframe</h2>
-		<img src="../images/wireframe-1.png" alt="Mobile wireframe for my portfolio." />
+		<img src="../images/wireframe-1-1.png" alt="Mobile wireframe for my portfolio." />
 		<h2>Desktop wireframe</h2>
-		<img src="../images/wireframe-2.png" alt="Desktop wireframe for my portfolio." />
+		<img src="../images/wireframe-2-1.png" alt="Desktop wireframe for my portfolio." />
 	</body>
 </html>
