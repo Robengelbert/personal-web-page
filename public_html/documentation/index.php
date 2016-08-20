850f5b3cfc3efaf986b0e45c89c13afe69c05179
Robert Engelbert <Robe32002@yahoo.com>
2016-08-20 09:40:05 -0600

    changed body and header colors

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -19,7 +19,7 @@
 	<body id="scroll">
 		<a id="home"></a>
 		<!-- navigation begins -->
-		<nav class="navbar navbar-light navbar-fixed-top" style="background-color: #538FEB">
+		<nav class="navbar navbar-light navbar-fixed-top" style="background-color: #725522">
 
 			<!-- Navbar brand-->
 			<a class="navbar-brand" href="#">Robert Engelbert</a>
