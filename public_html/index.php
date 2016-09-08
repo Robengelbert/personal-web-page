306fa28eca3f3e4b40ad79907e0c0e203ac28d14
Robert Engelbert <Robe32002@yahoo.com>
2016-09-08 11:08:27 -0600

    Updated my path for validate.js

--- public_html/index.php
+++ public_html/index.php
@@ -24,7 +24,7 @@
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
-		<script src="js/form-validate.js"></script>
+		<script src="javascript/form-validate.js"></script>
 
 		<!--		google recaptcha-->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
